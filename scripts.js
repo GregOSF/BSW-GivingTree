92763638d1f9bd9c0629dbb85c575cbb84712316
GregOSF <gregory.onza@gmail.com>
2015-11-03 17:01:20 -0800

    Edit hover logic

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
 $(function(){
 
-	      $('.tooltip-carousel').popover();
+	   //    $('.tooltip-carousel').popover();
 
-	      $('#carousel-example-generic').on('slide.bs.carousel', function () {
-	        $('.tooltip-carousel').popover('hide');
-	        $(this).find('.caraousel-tooltip-item.active').fadeOut(function(){
-	        $(this).removeClass('active');
-	        });
-	      });
+	   //    $('#carousel-example-generic').on('slide.bs.carousel', function () {
+	   //      $('.tooltip-carousel').popover('hide');
+	   //      $(this).find('.caraousel-tooltip-item.active').fadeOut(function(){
+	   //      $(this).removeClass('active');
+	   //      });
+	   //    });
 
-	      $('#carousel-example-generic').on('slid.bs.carousel', function () {
-	        var index = $(this).find('.carousel-inner > .item.active').index();
-	        $(this).find('.caraousel-tooltip-item').eq(index).fadeIn(function(){
-	        $(this).addClass('active');
-	        });
-	        //alert(index);
-	      });
+	   //    $('#carousel-example-generic').on('slid.bs.carousel', function () {
+	   //      var index = $(this).find('.carousel-inner > .item.active').index();
+	   //      $(this).find('.caraousel-tooltip-item').eq(index).fadeIn(function(){
+	   //      $(this).addClass('active');
+	   //      });
+	   //      //alert(index);
+	   //    });
 	  
-	  	$('.tooltip-carousel').mouseenter(function(){
-	      $(this).popover('show');
-	    }).mouseleave(function(){
-	      $(this).popover('hide');
-	    });
-
+	  	// $('.tooltip-carousel').mouseenter(function(){
+	   //    $(this).popover('show');
+	   //  }).mouseleave(function(){
+	   //    $(this).popover('hide');
+	   //  });
+	$('[data-toggle="tooltip"]').tooltip();
+	
 })
