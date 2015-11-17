ddc79c7b73ccfe7263bc0a1e466e4e632fb3bc75
GregOSF <gregory.onza@gmail.com>
2015-11-17 13:04:02 -0800

    Refactor for AngularJS

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,6 +1,31 @@
-$(function(){
+angular.module('givingTree', ['ngRoute'])
+
+
+	.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'templates/search.html',
+        controller: 'MainCtrl'
+      })
+
+      .when('/favorites', {
+        templateUrl: 'templates/favorites.html',
+        controller: 'FavoritesCtrl'
+      });
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  }])
+
+	.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+		    // controller logic
+		    $('[data-toggle="tooltip"]').tooltip();
+
+	}])
 
 	// BOOTSTRAP TOOLTIP
-	$('[data-toggle="tooltip"]').tooltip();
+	// $('[data-toggle="tooltip"]').tooltip();
 
-})
+
