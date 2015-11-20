1828843aa0e52314dfb529646c6fe626345f8f0b
GregOSF <gregory.onza@gmail.com>
2015-11-19 16:12:06 -0800

    add angular routing for pages

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,10 +1,22 @@
-angular.module('givingTree', ['ngRoute'])
+angular.module('givingTree', ['ngRoute','ngAnimate'])
 
 
 	.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: './public/views/templates/givingtree.html',
+        templateUrl: './public/views/templates/splash.html',
+        controller: 'MainCtrl'
+      })
+      .when('/water', {
+        templateUrl: './public/views/templates/water.html',
+        controller: 'MainCtrl'
+      })
+      .when('/education', {
+        templateUrl: './public/views/templates/education.html',
+        controller: 'MainCtrl'
+      })
+      .when('/teachers', {
+        templateUrl: './public/views/templates/teachers.html',
         controller: 'MainCtrl'
       });
   }])
@@ -12,10 +24,10 @@ angular.module('givingTree', ['ngRoute'])
 	.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 		    // controller logic
 		    $('[data-toggle="tooltip"]').tooltip();
-        var $map = $('#treemap')
-        $map.imageMapResize();
+        // var $map = $('#treemap')
+        // $map.imageMapResize();
+  }]);
         
 
-	}])
 
 
