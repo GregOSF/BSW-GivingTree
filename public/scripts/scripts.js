af99c4af1b13c4adaad3182a37d9f980b9a95d90
GregOSF <gregory.onza@gmail.com>
2015-11-19 18:41:48 -0800

    Edit/comment ngRoute

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,25 +1,25 @@
 angular.module('givingTree', ['ngRoute','ngAnimate'])
 
 
-	.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: './public/views/templates/splash.html',
-        controller: 'MainCtrl'
-      })
-      .when('/water', {
-        templateUrl: './public/views/templates/water.html',
-        controller: 'MainCtrl'
-      })
-      .when('/education', {
-        templateUrl: './public/views/templates/education.html',
-        controller: 'MainCtrl'
-      })
-      .when('/teachers', {
-        templateUrl: './public/views/templates/teachers.html',
-        controller: 'MainCtrl'
-      });
-  }])
+	// .config(['$routeProvider', function ($routeProvider) {
+ //    $routeProvider
+ //      .when('/', {
+ //        templateUrl: './public/views/templates/splash.html',
+ //        controller: 'MainCtrl'
+ //      })
+ //      .when('/water', {
+ //        templateUrl: './public/views/templates/water.html',
+ //        controller: 'MainCtrl'
+ //      })
+ //      .when('/education', {
+ //        templateUrl: './public/views/templates/education.html',
+ //        controller: 'MainCtrl'
+ //      })
+ //      .when('/teachers', {
+ //        templateUrl: './public/views/templates/teachers.html',
+ //        controller: 'MainCtrl'
+ //      });
+ //  }])
 
 	.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 		    // controller logic
